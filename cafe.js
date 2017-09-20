008a808832347798477a33400d3d3ff9200b04e9
Mike Charlton <mike.charlton@yourgolftravel.com>
2017-09-20 11:47:54 +0900

    Initial checkin

---
+++ cafe.js
@@ -0,0 +1,62 @@
+const Channel = require('./channel.js');
+
+const num_tourists = 25;
+const num_computers = 8;
+const min_time = 15;
+const max_time = 120;
+
+// Note: indexed from 1 because I am obtuse
+const range = num => {
+    return Array.from(new Array(num), (_, index) => index + 1);
+};
+
+const take = (array, num) => {
+    return [array.slice(0, num), array.slice(num)];
+};
+
+// Copied from MDN because I am lazy.
+const random_int = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max) + 1;
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const timeout = (minutes) => {
+    return new Promise((resolve, _) => {
+	setTimeout(() => resolve(), minutes);
+    });
+};
+
+const online = channel => async (tourist) => {
+    let current = tourist;
+    while(!channel.is_closed()) {
+	let time = random_int(min_time, max_time);
+	console.log('Tourist ' + current + ' is online.');
+	await timeout(time);
+	console.log('Tourist ' + current + ' is done, having spent ' +
+		    time + ' minutes online.');
+	current = await channel.read();
+    }
+    return Promise.resolve;
+};
+
+const waiting = channel => async (tourist) => {
+    console.log('Tourist ' + tourist + ' is waiting for turn.');
+    await channel.write(tourist.toString());
+    return Promise.resolve;
+};
+
+const computers = async(channel, tourists) => {
+    await Promise.all(tourists.map(online(channel)));
+    console.log("The place is empty, let's close up and go to the beach!");
+};
+
+const queue = async (channel, tourists) => {
+    await Promise.all(tourists.map(waiting(channel)));
+    channel.close();
+};
+
+const [head, tail] = take(range(num_tourists), num_computers);
+const channel = new Channel('/tmp/cafe');
+computers(channel, head);
+queue(channel, tail);
---
+++ channel.js
@@ -0,0 +1,87 @@
+const net = require('net');
+
+module.exports = class Channel {
+    constructor(filename) {
+        this.server = net.createServer((stream) => this.handleEvent(stream));
+        this.server.listen(filename);
+        this.wstream = net.connect(filename);
+        this.buffer = null;
+        this.writeNotify = null;
+	this.writePromise = null;
+        this.readNotify = null;
+    }
+
+    // Returns a promise which will resolve with the data sent to write
+    read() {
+        if(this.is_closed()) {
+	    return Promise.reject('Channel is closed');
+        }
+        if(this.buffer) {
+	    const readPromise = Promise.resolve(this.buffer);
+	    this.buffer = null;
+	    this.notifyWrite();
+	    return readPromise;
+        }
+        return new Promise((resolve, _) => {
+	    this.readNotify = resolve;
+        });
+    }
+
+    // Returns a promise which will resolve when data has been received
+    async write(data) {
+        if(this.is_closed()) {
+	    return Promise.reject('Channel is closed');
+        }
+	while(this.writePromise) {
+	    await this.writePromise;
+	}
+	this.wstream.write(data);
+        this.writePromise = new Promise((resolve, _) => {
+	    this.writeNotify = resolve;
+        });
+	return this.writePromise;
+    }
+
+    // Closes the channel
+    close() {
+        if(this.is_closed()) {
+	    return;
+        }
+        this.wstream.end();
+        this.wstram = null;
+    }
+
+    // Returns true if the channel has been closed
+    is_closed() {
+        return (!this.wstream);
+    }
+
+    // Private
+
+    // Handle an event on the read stream of the channel
+    handleEvent(stream) {
+        stream.on('data', (data) => this.receive(data));
+        stream.on('end', () => this.server.close());
+    }
+
+    // Notify the reader and writer that the data has arrived
+    // I *think* because the read promise is resolved before the write promise,
+    // node will return from the read await before the write await.
+    receive(data) {
+	// If nobody has read yet, buffer the data
+        if(!this.readNotify) {
+	    this.buffer = data.toString();
+	    return;
+        }
+        this.readNotify(data.toString());
+        this.readNotify = null;
+	this.notifyWrite();
+    }
+
+    notifyWrite() {
+        this.writeNotify();
+        this.writeNotify = null;
+	this.writePromise = null;
+    }
+};
+
